e244aee5a6adfaa02eba5ba4bedbf22473b731ab
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-24 08:39:31 -0600

    update finished, bonus not completed

--- main.js
+++ main.js
@@ -1,45 +1,55 @@
-/**************************  Victim Prompt    *************************/
+/**************************  Victim Prompt    *****************************/
 
 
 var getVictims = prompt("How many disaster victims are there?");
 
 
 
-/**************************   Victim Infp Prompt    *************************/
-
-for(var i = 0; i < getVictims; i ++ ) { 
- 
- 	    var arrary = [];
-	    vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: "));
-	    vicNam.push(prompt("Enter the NUMBER of victim number: " + i + " here: ");
-	    vicNam.push(prompt("Enter the STREET of victim number: " + i + " here: ");
-		};
-
-
-	var vicNam = [];
+/**************************   Victim Infp Prompt   ***************************/
+    var vicNam = [];  
 	var vicNum = [];
 	var vicStreet = [];
 
+for(var i = 0; i < getVictims; i ++ ) { 
+ 
+ 	//this wouldn't work when the array was below the 'for' loop
+	vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: ") );
+	vicNum.push(prompt("Enter the NUMBER of victim number: " + i + " here: ") );
+	vicStreet.push(prompt("Enter the STREET of victim number: " + i + " here: ") );
+		};
 
-/**************************  Volunteer Prompt   *************************/
 
-var getVictims = prompt("How many disaster victims are there?");
+
+/**************************  Volunteer Prompt   ******************************/
+
+var getVolunteers = prompt("How many volunteers are there?");
 
 
 /**************************   Volunteer Info Prompt    *************************/
+	var volNam = [];
+	var volNum = [];
+	var volStreet = [];
 
-for(var i = 0; i < getVictims; i ++ ) { 
+
+
+for(var i = 0; i < getVolunteers; i ++ ) { 
  
- 	    var arrary = [];
-	    vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: "));
-	    vicNam.push(prompt("Enter the NUMBER of victim number: " + i + " here: ");
-	    vicNam.push(prompt("Enter the STREET of victim number: " + i + " here: ");
+ 	volNam.push(prompt("Enter the NAME of volunteer number: " + i + " here: ") );
+	volNum.push(prompt("Enter the NUMBER of volunteer number: " + i + " here: ") );
+	volStreet.push(prompt("Enter the STREET of volunteer number: " + i + " here: ") );
     	
 		};
 
 
-	var vicNam = [];
-	var vicNum = [];
-	var vicStreet = [];
+	
+/*******************************  Alert   ***********************************/
 
-/**************************  Alert    *************************/
+var endAlert = alert( "Victim count: " + getVictims + "\n" + "Volunteers count: " + getVolunteers +
+ "\n" + "The victim names are: " + vicNam + "\n" + "The volunteer names are: " + volNam );
+
+
+/*******************************  Bonus Prompt  *********************************/
+
+// var vicNamPrompt = prompt("Enter the NAME of any victim");
+
+// if( vicNamPrompt = )
