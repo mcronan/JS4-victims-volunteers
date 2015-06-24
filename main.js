d20827d7e9e7e21a67267b07f4c19944066a0a87
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-23 19:15:39 -0600

    victims and volunteers

---
+++ main.js
@@ -0,0 +1,45 @@
+/**************************  Victim Prompt    *************************/
+
+
+var getVictims = prompt("How many disaster victims are there?");
+
+
+
+/**************************   Victim Infp Prompt    *************************/
+
+for(var i = 0; i < getVictims; i ++ ) { 
+ 
+ 	    var arrary = [];
+	    vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: "));
+	    vicNam.push(prompt("Enter the NUMBER of victim number: " + i + " here: ");
+	    vicNam.push(prompt("Enter the STREET of victim number: " + i + " here: ");
+		};
+
+
+	var vicNam = [];
+	var vicNum = [];
+	var vicStreet = [];
+
+
+/**************************  Volunteer Prompt   *************************/
+
+var getVictims = prompt("How many disaster victims are there?");
+
+
+/**************************   Volunteer Info Prompt    *************************/
+
+for(var i = 0; i < getVictims; i ++ ) { 
+ 
+ 	    var arrary = [];
+	    vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: "));
+	    vicNam.push(prompt("Enter the NUMBER of victim number: " + i + " here: ");
+	    vicNam.push(prompt("Enter the STREET of victim number: " + i + " here: ");
+    	
+		};
+
+
+	var vicNam = [];
+	var vicNum = [];
+	var vicStreet = [];
+
+/**************************  Alert    *************************/
