1fb3a3c9666d9731db5f731988b6b7302b61533c
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-24 19:08:06 -0600

    updates

--- main.js
+++ main.js
@@ -1,55 +1,88 @@
 /**************************  Victim Prompt    *****************************/
 
 
-var getVictims = prompt("How many disaster victims are there?");
+// var getVictims = prompt("How many disaster victims are there?");
 
 
 
-/**************************   Victim Infp Prompt   ***************************/
-    var vicNam = [];  
-	var vicNum = [];
-	var vicStreet = [];
+/**************************   Victim Inf Prompt   ***************************/
+//  var vicNam = [];  
+// 	var vicNum = [];
+// 	var vicStreet = [];
 
-for(var i = 0; i < getVictims; i ++ ) { 
+// for(var i = 0; i < getVictims; i ++ ) { 
  
  	//this wouldn't work when the array was below the 'for' loop
-	vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: ") );
-	vicNum.push(prompt("Enter the NUMBER of victim number: " + i + " here: ") );
-	vicStreet.push(prompt("Enter the STREET of victim number: " + i + " here: ") );
-		};
+	// vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: ") );
+	// vicNum.push(prompt("Enter the NUMBER of victim number: " + i + " here: ") );
+	// vicStreet.push(prompt("Enter the STREET of victim number: " + i + " here: ") );
+	// 	};
 
 
 
 /**************************  Volunteer Prompt   ******************************/
 
-var getVolunteers = prompt("How many volunteers are there?");
+// var getVolunteers = prompt("How many volunteers are there?");
 
 
 /**************************   Volunteer Info Prompt    *************************/
-	var volNam = [];
-	var volNum = [];
-	var volStreet = [];
+// 	var volNam = [];
+// 	var volNum = [];
+// 	var volStreet = [];
 
 
 
-for(var i = 0; i < getVolunteers; i ++ ) { 
+// for(var i = 0; i < getVolunteers; i ++ ) { 
  
- 	volNam.push(prompt("Enter the NAME of volunteer number: " + i + " here: ") );
-	volNum.push(prompt("Enter the NUMBER of volunteer number: " + i + " here: ") );
-	volStreet.push(prompt("Enter the STREET of volunteer number: " + i + " here: ") );
+ // 	volNam.push(prompt("Enter the NAME of volunteer number: " + i + " here: ") );
+	// volNum.push(prompt("Enter the NUMBER of volunteer number: " + i + " here: ") );
+	// volStreet.push(prompt("Enter the STREET of volunteer number: " + i + " here: ") );
     	
-		};
+	// 	};
 
 
 	
-/*******************************  Alert   ***********************************/
+/*******************************  End Alert   ***********************************/
 
-var endAlert = alert( "Victim count: " + getVictims + "\n" + "Volunteers count: " + getVolunteers +
- "\n" + "The victim names are: " + vicNam + "\n" + "The volunteer names are: " + volNam );
+// var endAlert = alert( "Victim count: " + getVictims + "\n" + "Volunteer count: " + getVolunteers +
+//  "\n" + "The victim names are: " + vicNam + "\n" + "The volunteer names are: " + volNam );
 
 
-/*******************************  Bonus Prompt  *********************************/
+/*******************************  Bonus I  *********************************/
 
-// var vicNamPrompt = prompt("Enter the NAME of any victim");
+// prompt users to input information of the victims right away
+// ask them if they want to continue
+// use while loop, confirm button, and the prompt button to achieve this
 
-// if( vicNamPrompt = )
+var vicNam = [];  
+var vicNum = [];
+var vicStreet = [];
+var vicLoop = function() {
+
+var i = 1;
+// when this is the only condition (!== true), the loops ends when this condition changes
+// this works, but why not when set to ==true?
+while (confirmButton !== false ) {
+	
+
+	vicNam.push(prompt("Enter the NAME of victim number: " + i + " here: ") );
+	vicNum.push(prompt("Enter the NUMBER of victim number: " + i + " here: ") );
+	vicStreet.push(prompt("Enter the STREET of victim number: " + i + " here: ") );
+	var confirmButton = confirm("Would you like to continue?"); 
+// when this is the second condition (and first condition !==true), the loop continues when confirming and cancelling
+	// confirmButton = false;
+
+	i += 1;
+		
+	}	
+
+	return vicLoop; 
+}
+
+vicLoop(); 
+
+
+
+
+
+
